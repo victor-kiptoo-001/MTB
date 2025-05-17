b7cb55e25a041264b21b3e63912db4ee542239c7
victor-kiptoo-001 <veelangat680@gmail.com>
2025-05-17 10:45:09 +0300

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'config/app_routes.dart';
+
+class ModelTownBoardApp extends StatelessWidget {
+  const ModelTownBoardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Model Town Board',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: AppRoutes.routes,
+    );
+  }
+}
