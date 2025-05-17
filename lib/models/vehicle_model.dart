af771ceb3c698620e9a1c7118b7617ae51e770ac
victor-kiptoo-001 <veelangat680@gmail.com>
2025-05-17 10:51:39 +0300

    firebase

---
+++ lib/models/vehicle_model.dart
@@ -0,0 +1,13 @@
+class VehicleModel {
+  final String id;
+  final String type;
+  double x;
+  double y;
+
+  VehicleModel({
+    required this.id,
+    required this.type,
+    required this.x,
+    required this.y,
+  });
+}
